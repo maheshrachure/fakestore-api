6f871bd88c535ea25a39fdc422c4de4e541d2120
mahesh rachure <rachuremahesh@gmail.com>
2024-03-11 10:45:05 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Products from "./components/Products";
+import ProductDetails from "./components/ProductDetails";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Products />} />
+        <Route exact path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
